0f8bff03e8954a42c0f994162842ad1264d6d79a
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-27 15:19:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,36 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+    @correct_word = []
+    
+  end
+  
+  
+  def match(words)
+    original_word = @word.split("")
+    
+    words.each do |word|
+      split_word = word.split("")
+      original_word = original_word.sort
+      split_word = split_word.sort 
+      if split_word == original_word
+        @correct_word << word
+      else
+        @correct_word.flatten
+      end
+    end
+    @correct_word.flatten
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+end
