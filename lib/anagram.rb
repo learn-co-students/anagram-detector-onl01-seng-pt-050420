e118e9d5049bc63897a3349a8b071534a5c9a8d2
Dang Nguyen <danghwin@gmail.com>
2020-05-27 22:52:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram 
+  
+  attr_accessor :words 
+  
+  def initialize(words)
+    @words = words
+    end 
+    
+    def match(w_array)
+      w_array.select do |words|
+        @words.split("").sort == words.split("").sort
+      end 
+    end 
+  end 
+
