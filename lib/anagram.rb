f6dea2dfdcfe5496ed99eda9c1a3224e7159df03
ValentinaPanic <valentina.panic87@gmail.com>
2020-05-28 17:39:55 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :words
+    
+    def initialize(word)
+        @word =  word
+    end
+        
+    def match(anagrams)
+        
+            anagrams.select{ |anagram| 
+            anagram.split("").sort.join == @word.split("").sort.join }
+           
+            
+    end                
+    
+
+end
