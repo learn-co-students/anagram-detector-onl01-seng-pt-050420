e6c87088b2152ef4608534d2b35d79ed8b18a7b1
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-05-29 14:08:34 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        # return the matches in an array
+        
+        anagrams.reject {|match| 
+            if match != word.reverse
+                match
+            else
+                match
+            end
+        }
+        
+    end
+
+end
