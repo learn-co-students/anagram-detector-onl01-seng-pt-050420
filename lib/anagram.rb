f257ff489ef7d8c92bea68e8902e868d0804f20a
Billy Ferrara <willfira717@gmail.com>
2020-05-31 19:20:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,19 @@
+# class Anagram
+#
+#   attr_accessor :word
+#
+#   def initialize(word)
+#     @word = word
+#   end
+#
+#   def match(array_of_words)
+#     array_of_words.select do |words|
+#       (@word.split("").sort) == (words.split("").sort)
+#     end
+#
+#   end
+#
+# end
 class Anagram
 
   attr_accessor :word
@@ -6,11 +22,11 @@ class Anagram
     @word = word
   end
 
-  def match(array_of_words)
-    array_of_words.select do |words|
-      (@word.split("").sort) == (words.split("").sort)
-    end 
-
+  def match(new_array)
+    new_array.select do |new_words|
+      (@word.split("").sort) == (new_words.split("").sort)
+    end
   end
+
 
 end
