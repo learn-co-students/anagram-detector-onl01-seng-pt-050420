8f7eb6c528ac2ab737e76f4273c19c8546393052
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-05-31 19:58:23 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram 
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_array)
+        words_array.select{ |match| match.split("").sort == word.split("").sort} 
+        #.select uses boolean values to filter your array!
+    end
+end
