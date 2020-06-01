3db94647d59df35914b78c05f8dce9bed2babd94
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-01 18:00:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+end
+
+def match(word_match)
+  array = []
+  word_match.each do |word|
+  if word.split("").sort == @word.split("").sort
+    array << word
+  end
+ end
+  return array 
+end
+end
+
+
+
